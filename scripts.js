7c1684bf5b5a09fdfa29fe03a988006f92b4477b
Filipe <filipe.m.mir@gmail.com>
2017-08-01 16:08:36 -0400

    Position tooltip duynamically

--- scripts.js
+++ scripts.js
@@ -11,8 +11,27 @@ $('.tab').on('click', function() {
 });
 
 // Tooltip setup
+function placeTooltipBelowElement({ selector }) {
+    const element = window.document.querySelectorAll(selector)[0],
+        tt = window.document.querySelectorAll('.tooltip')[0];
+
+    if (!element || !tt) {
+        return;
+    }
+
+    const ttBounds = tt.getBoundingClientRect(),
+        elementBounds = element.getBoundingClientRect(),
+        left = elementBounds.left + elementBounds.width/2 - ttBounds.width/2,
+        top = elementBounds.top + elementBounds.height + 7;  // 7 is the size of the arrow
+
+    tt.style.left = left;
+    tt.style.top = top;
+}
+
 $('.extra-fields input').on('focus', function() {
     $('.tooltip').show();
+
+    placeTooltipBelowElement({ selector: '.extra-fields' });
 });
 
 // $('.tooltip').show();
